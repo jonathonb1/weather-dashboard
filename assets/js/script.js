1c134e5c21948f31e36fbab5d3dc9cfe9165f7a1
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-08-18 16:00:31 -0700

    added api call successfully

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,27 @@
 $(document).ready(function () {
 
     // listen for button click within city search
-    $("#searchBtn").on("click", function () {
-        console.log('test')
+    $("#search-btn").on("click", function () {
+
+        // get search-value
+        const searchValue = $("#search-value").val().trim();
+
+        // call search weather function
+        searchWeather(searchValue);
     })
-    // get search-value
-    const searchValue = $("#search-value").val().trim();
+
+    // fucnction for todays weather
+    function searchWeather(cityName) {
+
+        // query api
+        $.ajax({
+            type: "GET",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=688988e3b9c619e778927f53f7761d5d`
+        }).then(function (response) {
+            console.log(response);
+        })
+    }
+
 })
 
 
